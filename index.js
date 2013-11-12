cc41f03eff7cac424431650485452ccacd6585b8
psirenny <djtorres0@gmail.com>
2013-11-12 09:58:08 -0500

    add config to local strategy

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = function (app, options) {
+  if (!options) options = {};
   return {
     init: require('./lib/init')(app, options),
     routes: require('./lib/routes')(app, options)
