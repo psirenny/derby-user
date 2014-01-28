30f6b25b682302da3072df753c0d3df7a3014a16
psirenny <djtorres0@gmail.com>
2014-01-28 08:07:51 -0500

    separate app and server

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-module.exports = function (app, options) {
-  if (!options) options = {};
-  return {
-    init: require('./lib/init')(app, options),
-    routes: require('./lib/routes')(app, options)
-  }
-};
+exports.app = require('./lib/app');
+exports.server = require('./lib/server');
